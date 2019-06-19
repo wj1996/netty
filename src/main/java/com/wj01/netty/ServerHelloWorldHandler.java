16a1340b99e0fc273abc93175cda4b9945167d9d
jie8023yu <2425393776@qq.com>
2019-06-20 00:28:34 +0800

    netty 入门

---
+++ src/main/java/com/wj01/netty/ServerHelloWorldHandler.java
@@ -0,0 +1,56 @@
+package com.wj01.netty;
+
+
+import io.netty.channel.ChannelHandler;
+import io.netty.channel.ChannelHandlerAdapter;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandler;
+
+@ChannelHandler.Sharable
+public class ServerHelloWorldHandler implements ChannelInboundHandler {
+
+
+    public void channelRegistered(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+    public void channelUnregistered(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+    public void channelActive(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+    public void channelInactive(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+
+    }
+
+    public void channelReadComplete(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+    public void userEventTriggered(ChannelHandlerContext ctx, Object evt) throws Exception {
+
+    }
+
+    public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+    public void handlerAdded(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+    public void handlerRemoved(ChannelHandlerContext ctx) throws Exception {
+
+    }
+
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+
+    }
+}
